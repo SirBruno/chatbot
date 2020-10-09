6881e097c83e528e698f1a466acc1b3a8ca1a9de
Bruno <bruno.webjr@gmail.com>
2020-10-08 23:08:42 -0300

    Added an example response containing options

--- src/App.js
+++ src/App.js
@@ -5,25 +5,47 @@ import botImg from './assets/botImg.png';
 
 function App() {
 
-  const [value, setValue] = useState(null);
+  const [lastMsg, setLastMsg] = useState(null);
 
   return (
     <div className="App">
-      <p>{value}</p>
-      <ChatBot width="400px" botAvatar={botImg}
+      <p>{lastMsg}</p>
+      <ChatBot width="600px" botAvatar={botImg}
         steps={[
           {
             id: '1',
-            message: 'Qual a boa de hoje, chefe?',
+            message: 'Olá, como posso ajudar?',
             trigger: '2',
           },
           {
             id: '2',
+            options: [
+              { value: 1, label: 'Orçamento', trigger: '2' },
+              { value: 2, label: 'Serviços', trigger: '2' },
+              { value: 3, label: 'Valores', trigger: '3' },
+            ],
+            trigger: '2',
+          },
+          {
+            id: '3',
+            component: (
+              <div>
+                <div><b>Site institucional (5 pg.):</b><br />R$ 3.000 + R$ 300 p/ página</div>
+                <br />
+                <div><b>E-commerce (100 prod.):</b><br />R$ 7.000 + R$ 10 p/ produto</div>
+                <br />
+                <div><b>Landing page:</b><br />R$ 1.400</div>
+              </div>
+            ),
+            trigger: '2',
+          },
+          {
+            id: '4',
             user: true,
             trigger: '3',
           },
           {
-            id: '3',
+            id: '5',
             message: ({ previousValue, steps }) => {
               console.log(previousValue);
               if (previousValue.toLowerCase() === 'bruno') {
