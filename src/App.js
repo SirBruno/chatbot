59b294990f9cd8a9a00a838d2355308917a62df2
Bruno <bruno.webjr@gmail.com>
2020-11-03 21:44:39 -0300

    Added a few new options and did some UX improvements

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import ChatBot from 'react-simple-chatbot';
 import botImg from './assets/botImg.png';
 
 function App() {
 
-  const [lastMsg, setLastMsg] = useState(null);
+  // const [lastMsg, setLastMsg] = useState(null);
+  // const [quote, setQuote] = useState({ graphicDesign: 0 });
 
   return (
     <div className="App">
-      <p>{lastMsg}</p>
+      {/* <p>{lastMsg}</p> */}
       <ChatBot width="600px" botAvatar={botImg}
         steps={[
           {
-            id: '100',
+            id: '1',
             message: 'Olá, como posso ajudar?',
-            trigger: '200',
+            trigger: '2',
           },
           {
-            id: '200',
+            id: '2',
             options: [
-              { value: 1, label: 'Orçamento', trigger: '500' },
-              { value: 2, label: 'Serviços', trigger: '600' },
-              { value: 3, label: 'Valores', trigger: '300' },
+              { value: 1, label: 'Orçamento', trigger: '5' },
+              { value: 2, label: 'Serviços', trigger: '6' },
+              { value: 3, label: 'Valores', trigger: '3' },
             ],
-            trigger: '200',
+            trigger: '2',
           },
           {
-            id: '300',
+            id: '3',
             component: (
               <div>
                 <div><b>Site institucional (5 pg.):</b><br />R$ 3.000 + R$ 300 p/ página</div>
@@ -37,15 +38,15 @@ function App() {
                 <div><b>Landing page:</b><br />R$ 1.400</div>
               </div>
             ),
-            trigger: '200',
+            trigger: '2',
           },
           {
-            id: '400',
+            id: '4',
             user: true,
-            trigger: '300',
+            trigger: '3',
           },
           {
-            id: '500',
+            id: '5',
             message: 'Seu site precisa de funcionalidades que o WordPress não possui nativamente?',
             // message: ({ previousValue, steps }) => {
             //   console.log(previousValue);
@@ -61,44 +62,91 @@ function App() {
             //     return `legal`
             //   } else return `fodase kkkkkkkk`
             // },
-            trigger: '501'
+            trigger: '17'
           },
           {
-            id: '501',
+            id: '17',
             options: [
-              { value: 1, label: 'Sim', trigger: '500' },
-              { value: 2, label: 'Não', trigger: '600' },
-              { value: 3, label: 'Não sei dizer', trigger: '502' },
+              { value: 1, label: 'Sim', trigger: '5' },
+              { value: 2, label: 'Não', trigger: '7' },
+              { value: 3, label: 'Não sei dizer', trigger: '16' },
             ],
-            trigger: '501'
+            trigger: '17'
           },
           {
-            id: '502',
+            id: '16',
             message: 'Se o seu site precisa de funcionalidades que exigem programação avançada, como um sistema de login customizado, meios de pagamento não presentes no WooCommerce, integrações que não podem ser feitas por meio de plugins já existentes na loja, ou qualquer coisa que envolva muito código, nós não poderemos te ajudar, uma vez que não trabalhamos com desenvolvimento de software. Esse é o caso do seu site?',
-            trigger: '503'
+            trigger: '15'
           },
           {
-            id: '503',
+            id: '15',
             options: [
-              { value: 1, label: 'Sim', trigger: '504' },
-              { value: 2, label: 'Não', trigger: '504' }
-            ],
-            trigger: '503'
+              { value: 1, label: 'Sim', trigger: '8' },
+              { value: 2, label: 'Não', trigger: '7' }
+            ]
           },
           {
-            id: '504',
+            id: '8',
             message: 'Desculpe, infelizmente não poderemos te ajudar.',
-            trigger: '200'
+            trigger: '2'
           },
           {
-            id: '600',
+            id: '7',
+            message: 'O site já possui design (mockup, protótipo ou stylescape)?',
+            trigger: '9'
+          },
+          {
+            id: '9',
+            options: [
+              { value: 1, label: 'Sim', trigger: '8' },
+              { value: 2, label: 'Não', trigger: '10' }
+            ]
+          },
+          {
+            id: '10',
+            message: 'Deseja contratar um serviço de design gráfico para a criação do design?',
+            trigger: '11'
+          },
+          {
+            id: '11',
+            options: [
+              { value: 1, label: 'Sim', trigger: '12' },
+              { value: 2, label: 'Não', trigger: '12' }
+            ]
+          },
+          {
+            id: '12',
+            message: 'Sua marca já possui logo?',
+            trigger: '13'
+          },
+          {
+            id: '13',
+            options: [
+              { value: 1, label: 'Sim', trigger: '12' },
+              { value: 2, label: 'Não', trigger: '14' }
+            ]
+          },
+          {
+            id: '14',
+            message: 'Deseja contratar um serviço de criação de logo?',
+            trigger: '18'
+          },
+          {
+            id: '18',
+            options: [
+              { value: 1, label: 'Sim', trigger: '12' },
+              { value: 2, label: 'Não', trigger: '14' }
+            ]
+          },
+          {
+            id: '6',
             component: (
               <div>
                 <div><b>Nossos serviços</b></div>
                 <div>Nós ofereçemos serviços de desenvolvimento web e design gráfico.</div>
               </div>
             ),
-            trigger: '200'
+            trigger: '14'
           }
         ]}
       />
