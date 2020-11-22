6f5035238a664614f7cc479dfd41029a86532a1f
Bruno <bruno.webjr@gmail.com>
2020-11-21 23:18:03 -0300

    Masked some price values that had no mask

--- src/GetPrice.js
+++ src/GetPrice.js
@@ -10,9 +10,9 @@ export default function GetPrice(props) {
     <div>
       <div><b>Site institucional (10 pg.):</b><br />R$ <span className="money">{props.websitePrice}</span> + R$ <span className="money">{props.pagePrice}</span> p/ página adicional</div>
       <br />
-      <div><b>E-commerce (100 prod.):</b><br />R$ 7.000 + R$ 10 p/ produto adicional</div>
+      <div><b>E-commerce (100 prod.):</b><br />R$ <span className="money">{props.ecommercePrice}</span> + R$ <span className="money">{props.prodPrice}</span> p/ produto adicional</div>
       <br />
-      <div><b>Landing page:</b><br />R$ 1.400</div>
+      <div><b>Landing page:</b><br />R$ <span className="money">{props.landingPagePrice}</span></div>
     </div>
   )
 }
