b539ca70e79ad40f21896cdf88fe436ec612dffc
Bruno <bruno.webjr@gmail.com>
2020-11-24 23:20:36 -0300

    Rebuilt the counting scheme and added some new features

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@ const utils = {
     $('.money').mask('000.000.000.000.000,00', { reverse: true });
   },
   formatNum: (n) => n.toFixed(2).replace(/.([^.]*)$/, ",$1"),
+  formatPersonName: (name) => name.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase()),
   websitePrice: 5000.00,
   ecommercePrice: 12000.00,
   pagePrice: 450.00,
